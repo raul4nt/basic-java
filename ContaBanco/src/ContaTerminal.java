357243b1c0761025c6edf40d1c419e1c43680b7a
Raul Silveira <raulantsilveira@gmail.com>
2025-09-10 16:06:02 -0300

    feat: create new project ProcessoSeletivo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -2,13 +2,6 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        //TODO: Conhecer e importar a classe Scanner
-
-        //Exibir as mensagens para o nosso usuário 
-
-        //Obter pela scanner os valores digitados no terminal 
-
-        //Exibir a mensagem conta criada
 
         Scanner scanner = new Scanner(System.in);
 
