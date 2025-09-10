23092c08b6843762134528421166c3c4b355598e
Raul Silveira <raulantsilveira@gmail.com>
2025-09-10 16:21:50 -0300

    create Contador class

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,13 +1,35 @@
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
   public static void main(String[] args) {
-    analisarCandidato(1900.0)
-    analisarCandidato(2200.0);
-    analisarCandidato(2000.0);
+  }
+
+  static void imprimirSelecionados() {
+        String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "MONICA"};
   }
 
   static void selecaoCandidatos() {
     String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "MONICA", "MIRELA", "ANDRÉ", "PEDRO", "LUÍSA", "GUSTAVO"};
+
+    int candidatosSelecionados = 0;
+    int candidatoAtual = 0;
+    double salarioBase = 2000.0;
+
+    while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+      String candidato = candidatos[candidatoAtual];
+      double salarioPretendido = valorPretendido();
+
+      System.out.println("O candidato " + candidato + " solicitou este valor de salário " + salarioPretendido);
+      if (salarioBase >= salarioPretendido) {
+        System.out.println("O candidato " + candidato + " foi selecionado para a vaga.");
+        candidatosSelecionados++;
+      }
+      candidatoAtual++;
+    }
+  }
+
+  static double valorPretendido() {
+    return ThreadLocalRandom.current().nextDouble(1800, 2200);
   }
 
   static void analisarCandidato(double salarioPretendido) {
